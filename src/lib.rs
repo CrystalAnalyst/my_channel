8fdc41a4bb53cf52a556013dcea4f249fb653009
zkthernum <kevin91cooland@gmail.com>
2024-05-20 00:30:53 +0800

    add Drop for Channel and test: it_works

--- src/lib.rs
+++ src/lib.rs
@@ -15,23 +15,38 @@ use std::sync::{Condvar, Mutex};
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
+    // in_use: 表示通道是否已经被占用(有thread在写)
+    in_use: AtomicBool,
+    // ready : 表示通道里是否有可用的元素.
     ready: AtomicBool,
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
 
 impl<T> Channel<T> {
     /// constrcutor
     pub fn new() -> Self {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()),
+            in_use: AtomicBool::new(false),
             ready: AtomicBool::new(false),
         }
     }
 
-    /// Satefy: user, you should ensures `Once` Semantics
-    pub unsafe fn send(&self, message: T) {
-        (*self.message.get()).write(message);
+    /// use `in_use` to ensures that Only sned One message.
+    pub fn send(&self, message: T) {
+        if self.in_use.swap(true, std::sync::atomic::Ordering::Relaxed) {
+            panic!("can't send more than One msg!");
+        }
+        unsafe { (*self.message.get()).write(message) };
         self.ready.store(true, std::sync::atomic::Ordering::Release);
     }
 
@@ -40,10 +55,33 @@ impl<T> Channel<T> {
         self.ready.load(std::sync::atomic::Ordering::Relaxed)
     }
 
-    pub unsafe fn receive(&self) -> T {
+    pub fn receive(&self) -> T {
         if !self.ready.load(std::sync::atomic::Ordering::Acquire) {
             panic!("no message available!");
         }
-        (*self.message.get()).assume_init_read()
+        unsafe { (*self.message.get()).assume_init_read() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let channel = Channel::new();
+        let t = thread::current();
+        thread::scope(|s| {
+            s.spawn(|| {
+                channel.send("hello world!");
+                t.unpark();
+            });
+            while !channel.is_ready() {
+                thread::park();
+            }
+            assert_eq!(channel.receive(), "hello world!");
+        })
     }
 }
